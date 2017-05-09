4d058da12bc021aa237fa3dae6ae5d66fe730a12
JacksonPassos <jacksonguitar33@gmail.com>
2017-05-09 15:34:04 -0300

    inserido menu de navegação

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -3,25 +3,27 @@ include ("cabecalho.php");
 include ("conecta-banco.php");
 ?>
 <?php
+function insereProduto($conexao, $nome, $preco){
+	$query = "insert into produtos(nome, preco) values ('{$nome}', '{$preco}')";
+	return mysqli_query($conexao, $query);
+}
+
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
 
-$query = "insert into produtos(nome, preco) values ('{$nome}', '{$preco}')";
-
-if(mysqli_query($conexao, $query)){
-
+if(insereProduto($conexao, $nome, $preco)){
 ?>
 
-<div class="alert-success">
+<div class="text-success">
 	<h3>Produto <?= $nome; ?> adicionado com sucesso! Seu preço é R$ <?= $preco;?>. </h3>
 </div>
 
 <?php
 }else{
-
+	$msg = mysqli_error($conexao);
 ?>
-<div class="alert-danger">
-	<h3>Erro no cadastro do produto. Tente Novamente. </h3>
+<div class="text-danger">
+	<h3>Erro no cadastro do produto. <?= $msg ?> </h3>
 </div>
 
 <?php
--- produto-formulario.php
+++ produto-formulario.php
@@ -5,12 +5,20 @@ include ("cabecalho.php");
 <h2>Cadastro de Produto</h2>
 
 <form action="adiciona-produto.php">
-	Nome:
-	<input type="text" name="nome"><br/>
-	Preço:
-	<input type="number" name="preco"><br/>
-
-	<input class="btn btn-primary" type="submit" value="Cadastrar">
+	<table class="table">
+		<tr>
+			<td>Nome: </td>
+			<td><input class="form-control" type="text" name="nome"></td>
+		</tr>
+		<tr>
+			<td>Preço: </td>
+			<td><input class="form-control" type="number" name="preco"></td>
+		</tr>
+		<tr>
+			<td><input class="btn btn-primary" type="submit" value="Cadastrar"></td>
+		</tr>
+	</table>
+	
 </form>
 
 <?php 
