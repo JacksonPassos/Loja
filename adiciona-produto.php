201f69930aaf0a03361824ad09c0eec93a630e15
JacksonPassos <jacksonguitar33@gmail.com>
2017-05-09 14:38:00 -0300

    conexão com o banco de dados feita

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,17 +1,32 @@
 <?php 
 include ("cabecalho.php");
+include ("conecta-banco.php");
 ?>
 <?php
-
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
+
+$query = "insert into produtos(nome, preco) values ('{$nome}', '{$preco}')";
+
+if(mysqli_query($conexao, $query)){
 
 ?>
 
 <div class="alert-success">
-	<h3>Produto <?= $nome; ?> adicionado com sucesso!</h3>
+	<h3>Produto <?= $nome; ?> adicionado com sucesso! Seu preço é R$ <?= $preco;?>. </h3>
 </div>
 
+<?php
+}else{
+
+?>
+<div class="alert-danger">
+	<h3>Erro no cadastro do produto. Tente Novamente. </h3>
+</div>
+
+<?php
+}
+?>
 
 <?php 
 include ("rodape.php");
--- produto-formulario.php
+++ produto-formulario.php
@@ -2,7 +2,16 @@
 include ("cabecalho.php");
 ?>
 
+<h2>Cadastro de Produto</h2>
 
+<form action="adiciona-produto.php">
+	Nome:
+	<input type="text" name="nome"><br/>
+	Preço:
+	<input type="number" name="preco"><br/>
+
+	<input class="btn btn-primary" type="submit" value="Cadastrar">
+</form>
 
 <?php 
 include ("rodape.php");
